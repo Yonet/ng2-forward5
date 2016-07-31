e7ec97cb292086c6f3736c618cfe1a2792b1c1e9
Aysegul Yonet <aysegulyonet@gmail.com>
2016-07-31 01:10:43 -0700

    Material checkbox added.

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MdCheckbox } from '@angular2-material/checkbox';
 
 @Component({
   moduleId: module.id,
   selector: 'a360-lists',
   templateUrl: 'lists.component.html',
-  styleUrls: ['lists.component.css']
+  styleUrls: ['lists.component.css'],
+  directives: [MdCheckbox]
+
 })
 export class ListsComponent implements OnInit {
 
--- src/system-config.ts
+++ src/system-config.ts
@@ -9,10 +9,21 @@
  **********************************************************************************************/
 /** Map relative paths to URLs. */
 const map: any = {
+  '@angular2-material': 'vendor/@angular2-material'
 };
 
 /** User packages configuration. */
 const packages: any = {
+  '@angular2-material/core': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'core.js'
+  },
+  '@angular2-material/checkbox': {
+    format: 'cjs',
+    defaultExtension: 'js',
+    main: 'checkbox.js'
+  }
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
